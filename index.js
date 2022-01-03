5d46b3158deedd294373749c6a051037807d9a1d
dkant1 <dkcomc@gmail.com>
2022-01-03 18:14:56 +0530

    Added EJS templates

--- index.js
+++ index.js
@@ -10,19 +10,53 @@ import bookingRouter from "./routes/booking.js";
 import authRouter from "./routes/auth.js";
 import registerRouter from "./routes/register.js";
 
+import bcrypt from 'bcrypt';
+import pool from './db.js';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 dotenv.config();
 
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
-// app.get("/")--> GET on the root will be handled automatically by sending index.html from public folder
+
+app.get("/",(req,res)=>{
+  res.render("login.ejs");
+}); 
+
+// app.get("/login", (req,res)=>{
+//   res.render("login.ejs");
+// });
+
+// app.post("/login", async (req, res)=>{
+
+// });
+
+app.get("/register", (req,res)=>{
+  res.render("registration.ejs");
+});
+
+app.post("/register", async (req, res)=>{
+
+  try {
+    const hashedPwd = await bcrypt.hash(req.body.password,10);
+    await pool.query("INSERT INTO users(email, passcode, mobile) VALUES($1, $2, $3)",[ req.body.email, hashedPwd, req.body.mobile]);
+    res.redirect("/");
+    
+  } catch (error) {
+    res.redirect("/register");
+  }
+
+});
 
 app.use("/api/parkinglots", parkingLotRouter);
 app.use("/api/parkingslots", parkingSlotRouter);
 app.use("/api/bookings", bookingRouter);
 app.use("/api/auth",authRouter);
-app.use("/register", registerRouter);
+app.use("/api/register", registerRouter);
 
 
 app.get("*", (req, res) => {
